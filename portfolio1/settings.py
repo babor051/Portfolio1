82016d1f74fd8e4502c36464b20772c49e1222c1
Babor Islam <47164803+babor99@users.noreply.github.com>
2021-01-25 22:18:19 +0600

    Update settings.py

--- portfolio1/settings.py
+++ portfolio1/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tk^=a99e_h-e6s5l#^k57uh_ht)g%l!uz4o!oi=_+dw)_o4l0f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
